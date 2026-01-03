3bab013f67ab0ff27cb53e2e913a0b8285815bd9
luiseurban <165191210+luiseurban@users.noreply.github.com>
2026-01-02 20:13:13 -0500

    Crear endpoint ObtenerProductos

--- productos-inventario/src/routes/productos.routes.js
+++ productos-inventario/src/routes/productos.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { crearProducto } from "../controllers/producto.controller.js";
+import { obtenerProductos } from "../controllers/producto.controller.js";
 
 const router = Router();
 
 // Endpoint para crear un nuevo producto
 router.post("/", crearProducto);
+// Endpoint para obtener todos los productos
+router.get("/", obtenerProductos);
+
 
 export default router;
 
